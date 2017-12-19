133ed81a9fefb8c074b19f569179353da9adc253
teragt <teragt2@gmail.com>
2017-12-19 14:56:14 -0800

    first commit

---
+++ zundoko.ts
@@ -0,0 +1,22 @@
+    let zunCounter = 0;
+    let dokoFlag = 0;
+    
+    function getZunDoko(){
+
+        while(zunCounter <= 4 && dokoFlag === 0){
+            let zundon = Math.floor( Math.random() * 2);
+            
+            if(zundon===0){
+              console.log('ズン');
+              if(zunCounter < 4)zunCounter++;
+            }else if(zundon===1){
+              console.log('ドコ');
+              dokoFlag=0;
+              if(zunCounter < 4)zunCounter=0;
+              else if(zunCounter > 3)dokoFlag=1;
+            }
+        }
+        console.log('き・よ・し！');
+    }
+
+    getZunDoko();
