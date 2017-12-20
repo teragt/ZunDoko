92ae93766e902ee1d773d31a66d021df23c54d1b
teragt <teragt2@gmail.com>
2017-12-19 17:16:49 -0800

    no global

--- zundoko.ts
+++ zundoko.ts
@@ -1,25 +1,26 @@
-    let zunCounter = 0;
-    let dokoFlag = 0;
-    let count = 0;
-    
-    function getZunDoko(){
+    getZunDoko();
 
-        while(zunCounter <= 4 && dokoFlag === 0){
+    function getZunDoko(){
+        let zunCounter = 0;
+        let dokoFlag = false;
+        let count = 0;
+        
+        while(zunCounter <= 4 && dokoFlag === false){
             let zundon = Math.floor( Math.random() * 2);
             count++;
-            
-            if(zundon===0){
-              console.log('ズン');
-              if(zunCounter < 4)zunCounter++;
-            }else if(zundon===1){
-              console.log('ドコ');
-              dokoFlag=0;
-              if(zunCounter < 4)zunCounter=0;
-              else if(zunCounter > 3)dokoFlag=1;
-            }
+           
+             if(zundon===0){
+                 console.log('ズン');
+                 if(zunCounter < 4)zunCounter++;
+             }else if(zundon===1){
+               console.log('ドコ');
+               dokoFlag=false;
+               if(zunCounter < 4)zunCounter=0;
+               else if(zunCounter > 3)dokoFlag=true;
+             }else{
+                 console.log('ERROR!!!!!');
+             }
         }
         console.log('き・よ・し！');
         console.log(count + '回目で成功')
-    }
-
-    getZunDoko();
+}
