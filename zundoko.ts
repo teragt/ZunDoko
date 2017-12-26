c336e0e1f819b6bb6696cfdb1af284a3e4b21cfa
teragt <teragt2@gmail.com>
2017-12-26 18:26:24 +0900

    omikuji add

--- zundoko.ts
+++ zundoko.ts
@@ -11,13 +11,24 @@
             if(zundoko) {
                 console.log('ズン');
                 zun++;
-                zun_Counter < 4 ? zun_Counter++ : false;
+                zun_Counter < 4 ? zun_Counter++ : false;;
             } else {
                 console.log('ドコ');
                 doko++;
                 zun_Counter < 4 ? zun_Counter=0 : kiyoshi_Flag=true;
             }
         }
-        console.log('き・よ・し！');
+
+        console.log('き・よ・し！\n');
         console.log(`${cnt}回目で成功\nズン:${zun}回\nドコ:${doko}回`)
+        
+        let result = cnt <= 5 ? "大吉 きよし"
+                   : cnt <= 20 ? "吉 きよし"
+                   : cnt <= 30 ? "中吉 きよし"
+                   : cnt <= 60 ? "小吉 きよし"
+                   : cnt === 77 ? "ラッキーよし"
+                   : cnt <= 80 ? "末吉 きよし"
+                   : cnt <= 120 ? "凶 きよし"
+                   : "大凶 きよし"
+        console.log(`\n【きよしおみくじ】\n${result}`);
     }
